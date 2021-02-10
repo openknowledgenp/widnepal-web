ddf87cfac711b43fb6b77ef069ba5dc1a5755ad2
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-10 18:46:47 +0545

    added semantic ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from 'next/app';
 import { ApolloProvider } from '@apollo/react-hooks';
+import 'semantic-ui-css/semantic.min.css'
 
 import withData from '../util/apollo-client';
 
