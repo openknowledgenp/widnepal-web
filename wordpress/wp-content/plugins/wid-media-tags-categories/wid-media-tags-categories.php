424c50fa35138448c76d4808c72b7e9124bc987b
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-13 12:34:56 +0545

    add tags and categories for media

---
+++ wordpress/wp-content/plugins/wid-media-tags-categories/wid-media-tags-categories.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package           WID custom tags and categories
+ * @author            Open Knowledge Nepal
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WID Custom tags and categories plugin
+ * Description:       Plugins for events/resource
+ * Version:           0.1
+ */
+
+// apply categories to attachments
+
+function add_media_cats() {
+
+  register_taxonomy_for_object_type( 
+
+    'category', 
+
+    'attachment' 
+
+  );
+
+}
+
+add_action( 'init' , 'add_media_cats' );
+
+// apply tags to attachments
+
+function add_media_tags() {
+
+  register_taxonomy_for_object_type( 
+
+    'post_tag', 
+
+    'attachment' 
+
+  );
+
+}
+
+add_action( 'init' , 'add_media_tags' );
