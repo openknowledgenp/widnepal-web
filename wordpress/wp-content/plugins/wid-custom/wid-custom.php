5198d4a853aed4b767526c1d25659e7487cd7eca
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-17 16:05:37 +0545

    added resource menu Item

--- wordpress/wp-content/plugins/wid-custom/wid-custom.php
+++ wordpress/wp-content/plugins/wid-custom/wid-custom.php
@@ -10,15 +10,22 @@
  * Version:           0.1
  */
 
+// Remove some default menu items
+require_once('remove_menu_items.php');
+
 // Allow tags and Category in media
 require_once( 'media_taxonomy.php' );
 
 // Add event post type
-require_once( 'event_menuitem.php' );
+require_once( 'add_event_menuitem.php' );
+
+// Add resource post type
+require_once( 'add_resource_menuitem.php' );
 
 // add local fields
 if( function_exists('acf_add_local_field_group') ):
   require_once( 'event_form.php' );
+  // require_once( 'resource_form.php' );
   require_once( 'image_website_link.php' );
   require_once( 'pinned_resources.php' );
 endif;
