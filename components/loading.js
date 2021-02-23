3888bc7e68ca6d87aae9e6aae385c4d4c476bfe5
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-23 15:20:57 +0545

    changed the spinner

--- components/loading.js
+++ components/loading.js
@@ -3,7 +3,7 @@ import { SwishSpinner } from 'react-spinners-kit';
 export const Loading = () => {
   return(
       <div style={{height: '100vh', paddingTop:'24%', paddingLeft: '48%'}}>
-            <SwishSpinner size={20} color="#249DFB"/>
+            <SwishSpinner size={20} color="#00F" fontColor="#00F" backColor="#00F"/>
       </div>
   )
 }
