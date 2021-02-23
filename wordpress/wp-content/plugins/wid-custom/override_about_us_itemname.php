4f4ccf3c1de26837e0e05c7d26fd3655f32b6c4e
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-23 15:47:03 +0545

    changed the spinner

--- wordpress/wp-content/plugins/wid-custom/override_about_us_itemname.php
+++ wordpress/wp-content/plugins/wid-custom/override_about_us_itemname.php
@@ -5,11 +5,14 @@ function about_us_name_updater( $post_id ) {
   $my_post['ID'] = $post_id;
 
   if ( get_post_type() == 'about' ) {
-    if (strcmp('Learn About Us', strval(get_field('page'))) !== 0) {
+    if (strcmp('Committee Member', strval(get_field('page'))) == 0) {
       $my_post['post_title'] = get_field('page').' > '.get_field('insert_option');
     }
-    else {
+    elseif (strcmp('Learn About Us', strval(get_field('page'))) == 0) {
       $my_post['post_title'] = get_field('page');
+    }
+    elseif (strcmp('Other', strval(get_field('page'))) == 0) {
+      $my_post['post_title'] = get_field('page'). ' > '. get_field('page_title');
     }
   }
 
