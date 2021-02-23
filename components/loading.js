826798051fc7d60ebc83db88bbcad7147058fa9a
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-23 15:16:47 +0545

    changed the spinner

--- components/loading.js
+++ components/loading.js
@@ -1,9 +1,9 @@
-import { SpinnerDotted } from 'spinners-react';
+import { SwishSpinner } from 'react-spinners-kit';
 
 export const Loading = () => {
   return(
-      <div style={{height: '100vh'}}>
-            <SpinnerDotted size="10%" color="#249DFB" speed={200} style={{ marginTop:'20%', marginLeft: '45%',}}/>
+      <div style={{height: '100vh', paddingTop:'24%', paddingLeft: '48%'}}>
+            <SwishSpinner size={20} color="#249DFB"/>
       </div>
   )
 }
