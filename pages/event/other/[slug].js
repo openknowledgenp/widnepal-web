d97d1e0c66a91458f4ad241222896fa7cf619d8b
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-26 08:45:10 +0545

    completed upcoming event detail screen

--- pages/event/other/[slug].js
+++ pages/event/other/[slug].js
@@ -1,11 +1,11 @@
 import { useQuery } from '@apollo/react-hooks';
-import { POSTS } from '../../../graphql/event.queries';
+import { EVENT_WITH_SLUG } from '../../../graphql/event.queries';
 import { PageLayout } from '../../../components/pageLayout'
 import { Loading } from '../../../components/loading'
 
 const Event = () => {
   // Create a query hook
-  const { data, loading, error } = useQuery(POSTS);
+  const { data, loading, error } = useQuery(EVENT_WITH_SLUG);
   if (loading) {
     return <Loading />;
   }
