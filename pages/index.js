60b342b540716590a5a9d09954295a5adbbc4b88
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-28 00:29:03 +0545

    updated next config

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Home from './home';
 
 const MainPage = () => {
+  console.log(process.env.CONTACT_GMAIL_USER);
+  console.log(process.env.CONTACT_GMAIL_PASSWORD);
+  console.log(process.env.RECEIPIENT_MAIL);
   return (
     <Home/>
   );
