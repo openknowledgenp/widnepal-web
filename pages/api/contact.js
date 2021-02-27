ae308d84fcf00e42674b765f48937e16127fb027
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-28 00:45:32 +0545

    updated next config

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,31 +1,28 @@
 import nodemailer from "nodemailer"
 
 
-var transporter = nodemailer.createTransport({
-       service: "Gmail",
-       auth: {
-           user: process.env.CONTACT_GMAIL_USER,
-           pass: process.env.CONTACT_GMAIL_PASSWORD
-       }
-   });
+// var transporter = nodemailer.createTransport({
+//        service: "Gmail",
+//        auth: {
+//            user: process.env.CONTACT_GMAIL_USER,
+//            pass: process.env.CONTACT_GMAIL_PASSWORD
+//        }
+//    });
 
 
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.office365.com',
-//   port: 587,
-//   secureConnection: false,
-//   tls: { ciphers: 'SSLv3' },
-//   auth: {
-//     user: process.env.CONTACT_EMAIL_USER, // add user
-//     pass: process.env.CONTACT_EMAIL_PASSWORD, // add password
-//   }
-// })
+const transporter = nodemailer.createTransport({
+  host: 'smtp.office365.com',
+  port: 587,
+  secureConnection: false,
+  tls: { ciphers: 'SSLv3' },
+  auth: {
+    user: process.env.CONTACT_EMAIL_USER, // add user
+    pass: process.env.CONTACT_EMAIL_PASSWORD, // add password
+  }
+})
 
 
 export default async (req, res) => {
-    console.log(process.env.CONTACT_GMAIL_USER);
-    console.log(process.env.CONTACT_GMAIL_PASSWORD);
-    console.log(process.env.RECEIPIENT_MAIL);
     const { email, full_name, message, subject } = req.body
     const recipientMail = process.env.RECEIPIENT_MAIL
 
