90a9e7d5b38d472b8f3d5f715350bce4d000221b
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-02-27 22:58:04 +0545

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
+const withPlugins = require('next-compose-plugins');
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
 const withImages = require('next-images')
-module.exports = withImages()
+const withNextEnv = nextEnv();
+dotenvLoad();
+module.exports = withPlugins(
+  [
+    [withNextEnv, { /* plugin config here ... */ }],
+    [withImages,  { /* plugin config here ... */ }],
+  ],
+  {
+    /* global config here ... */
+  },
+);
